0f5b79f2a7b4450d7613a6ae37cf19539e909f3f
Thanh Wang <thanh.wang@oru.se>
2020-09-20 20:54:20 +0200

    added excel

--- tests/testthat/test-plotEIC.R
+++ tests/testthat/test-plotEIC.R
@@ -1,4 +1,13 @@
 test_that("plotting works", {
   
-  expect_equal(2 * 2, 4)
+  
+  featlist <- data.frame(name = c("susp1", "susp2", "susp3", "susp4", "susp5", "susp6"), 
+                         mz = c(247.9618, 298.9429, 505.0516, 348.9398, 79.9630, 79.9630), 
+                         ms_level = c("ms1", "ms1", "ms1", "ms1", "ms1", "ms2"))
+  
+  filepath <-  "D:/Raw_data/Kallinge/New_analysis_20200414/centroid/QA_QC_1_F,3_1.mzML"
+  
+  plotEIC(filepath = filepath, featlist = featlist)
+
+  
 })
