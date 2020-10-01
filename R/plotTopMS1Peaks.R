f753035cb76137b6491322d05b9e0e29b85933ce
Thanh Wang <thanh.wang@oru.se>
2020-10-01 15:01:47 +0200

    added full code for plotEIC

--- R/plotTopMS1Peaks.R
+++ R/plotTopMS1Peaks.R
@@ -44,12 +44,42 @@ plotTopMS1Peaks <- function(filepath, featlist, numTopIons = 10, diff = 0.01, ms
   server <- function(input, output, session) {
     
     data_prof <- MSnbase::readMSData(filepath, mode = "onDisk", centroided = TRUE)
+    hd <- MSnbase::fData(data_prof)
+    ms1 <- which(hd$msLevel == 1)
+    ms2 <- which(hd$msLevel == 2)
+    
+    rtselms1 <- hd$retentionTime[ms1] > hd$retentionTime[ms1][1] & hd$retentionTime[ms1] < tail(hd$retentionTime[ms1], n=1)
+    rtselms2 <- hd$retentionTime[ms2] > hd$retentionTime[ms2][1] & hd$retentionTime[ms2] < tail(hd$retentionTime[ms2], n=1)
+    
+    M_plot <- vector("list", nrow(featlist))
+    rtr <- NULL
+    rtselection <- NULL
     
     output$plot1 <- plotly::renderPlotly({
-      MSXploreR::plotEIC(filepath = filepath, featlist = featlist, diff = diff, mserr = mserr)
-    })  
+      p <- plotly::plot_ly() 
+      
+      for (i in 1:nrow(featlist)) {
+        M <- MSnbase::MSmap(data_prof, 
+                            scans = if(featlist$ms_level[i] == "ms1") {ms1[rtselms1]} 
+                            else if(featlist$ms_level[i] == "ms2") {ms2[rtselms2]}, 
+                            lowMz = featlist$mz[i]-diff, 
+                            highMz = featlist$mz[i]+diff, 
+                            resMz = mserr, 
+                            hd = hd, 
+                            zeroIsNA = FALSE)
+        M_plot[[i]] <- data.frame(rt = M@rt, int = M@map) %>%
+          replace(., is.na(.), 0) %>%
+          dplyr::mutate(sum_int = rowSums(.[grep("int", names(.))], na.rm = TRUE))
+        p <- plotly::add_lines(p, 
+                               x = M_plot[[i]][["rt"]], 
+                               y = M_plot[[i]][["sum_int"]], 
+                               name = paste0(round(featlist$mz[i],4), "_", featlist$ms_level[i])
+        ) # do not use $ dollar sign for subsetting list
+      }
+      p %>% plotly::config(showTips = FALSE) 
+      })  
     
-    rtr <- NULL
+    
 
    # Output the clicked point to be input in next plot
     shiny::observeEvent(plotly::event_data("plotly_click"), {
@@ -81,15 +111,35 @@ plotTopMS1Peaks <- function(filepath, featlist, numTopIons = 10, diff = 0.01, ms
       featlist <- featlist %>% dplyr::bind_rows(featlist)
       
       output$plot2 <- plotly::renderPlotly({
-        MSXploreR::plotEIC(filepath = fl, featlist = featlist, diff = diff, mserr = diff)
-      })
+        p <- plotly::plot_ly() 
+        
+        for (i in 1:nrow(featlist)) {
+          M <- MSnbase::MSmap(data_prof, 
+                              scans = if(featlist$ms_level[i] == "ms1") {ms1[rtselms1]} 
+                              else if(featlist$ms_level[i] == "ms2") {ms2[rtselms2]}, 
+                              lowMz = featlist$mz[i]-diff, 
+                              highMz = featlist$mz[i]+diff, 
+                              resMz = mserr, 
+                              hd = hd, 
+                              zeroIsNA = FALSE)
+          M_plot[[i]] <- data.frame(rt = M@rt, int = M@map) %>%
+            replace(., is.na(.), 0) %>%
+            dplyr::mutate(sum_int = rowSums(.[grep("int", names(.))], na.rm = TRUE))
+          p <- plotly::add_lines(p, 
+                                 x = M_plot[[i]][["rt"]], 
+                                 y = M_plot[[i]][["sum_int"]], 
+                                 name = paste0(round(featlist$mz[i],4), "_", featlist$ms_level[i])
+          ) # do not use $ dollar sign for subsetting list
+        }
+        p %>% plotly::config(showTips = FALSE) 
+      })  
       
     })
     
-    
+    # Get ranges of plotly 
     shiny::observeEvent(input$sync1, {
-      rtselection1 <- plotly::event_data("plotly_relayout")
-      print(rtselection1)
+      rtselection <- plotly::event_data("plotly_relayout")
+      print(rtselection)
     })
     
     
