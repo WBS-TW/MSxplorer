8f1a79b288ce0ec2cbf2cdc9800874ef2de5d073
WBS-TW <37210875+WBS-TW@users.noreply.github.com>
2022-04-07 10:10:30 +0200

    added msp converters

--- R/findhalo_msp.R
+++ R/findhalo_msp.R
@@ -7,82 +7,18 @@ library(plotly)
 
 #####functions#######
 
-#----findhalo_rt------
-findhalo_rt <- function(mz,
-                        intensity,
-                        sf = 79/78.917789,
-                        step = 0.001,
-                        stepsd1=0.003,
-                        stepsd2=0.005,
-                        mzc=700,
-                        cutoffint = 1000,
-                        cutoffr=0.4,
-                        rt,
-                        clustercf =5){
-  mzr <- round(mz)
-  sm <- mz*sf
-  sd <- ceiling(sm)-sm
-  smsd <- ifelse(mz<=mzc,stepsd1,stepsd2)
-  smstep <- seq(0,1,step)
-  rt <- rt
-  
-  data <- cbind.data.frame(mz=mz,
-                           mzr = mzr,
-                           sm = sm,
-                           sd =sd,
-                           intensity=intensity,
-                           rt = rt)
-  data2 <<- data
-  
-  result <- NULL
-  for(i in 1:length(smstep)){
-    maxi = smstep[i]+smsd
-    mini = smstep[i]-smsd
-    index = sd<maxi & sd>mini
-    
-    li <- data[index & intensity > cutoffint,]
-    mzt <- mzr[index & intensity > cutoffint]
-    rtt <- rt[index & intensity > cutoffint]
-    
-    if(length(mzt)>=2){
-      c <- cutree(hclust(dist(mzt)),h=clustercf)
-      t <- cutree(hclust(dist(rtt)), h = clustercf)
-      u <- paste0(c, t)
-      cn <- length(unique(u))
-      lit <- cbind.data.frame(li,u,i)
-      for (j in 1:cn){
-        li2 <- lit[lit[,7]==j,]
-        mzt2 <- lit$mzr[lit[,7]==j]
-        
-        if(length(mzt2)>=2){
-          if(length(unique(li2$intensity))>1){
-            ratio <- max(li2$intensity[li2$intensity != max(li2$intensity)]) / max(li2$intensity)
-            diff <- abs(li2$mzr[round(li2$intensity) == round(max(li2$intensity[li2$intensity != max(li2$intensity)]))] - li2$mzr[which.max(li2$intensity)])
-          }else{
-            ratio <- 1
-            diff <- abs(li2$mzr[1]-li2$mzr[2])
-          }
-          
-          if(ratio>cutoffr&round(diff)==2){
-            result <- rbind.data.frame(result,li2)
-          }
-        }
-      }
-    }
-  }
-  return(result[!duplicated(result$mz), ])
-}
 
 #----findhalo_no_rt---------
 findhalo_no_rt <- function(mz,
                            intensity,
                            sf = 79/78.917789,
                            step = 0.001,
-                           stepsd1=0.003,
-                           stepsd2=0.005,
-                           mzc=700,cutoffint = 1000,
+                           stepsd1=0.002,
+                           stepsd2=0.003,
+                           mzc=700,
+                           cutoffint = 1000,
                            cutoffr=0.4,
-                           clustercf = 5){
+                           clustercf = 3){
   mzr <- round(mz)
   sm <- mz*sf
   sd <- ceiling(sm)-sm
@@ -195,12 +131,11 @@ ui <- dashboardPage(
   dashboardSidebar(width = 300,
                    sidebarMenu(
                      fileInput("file1", "Select msp file"),
-                     checkboxInput("check_rt", "Include retention times"),
                      numericInput("msp_id", "Which msp id to read", value = 1, min = 1),
                      actionButton("click_file", "Select"),
                      fluidRow(
-                       column(4, checkboxInput("check_int", "Show intensity")),
-                       column(4, checkboxInput("check_overlay", "Overlay data"))
+                       column(4, checkboxInput("check_overlay", "Overlay data")),
+                       column(4, checkboxInput("norm_int", "Normalize intensity"))
                      ),
                      numericInput("md_nom", "Nomin mass", value = 79, width = "60%"),
                      numericInput("md_exact", "Exact mass", value = 78.917789),
@@ -233,22 +168,6 @@ server <- function(input, output, session){
   
   
   result <- reactive({
-    if(input$check_rt){
-      raw_data <- raw_data()
-      t <- findhalo_rt(mz = raw_data$mz, 
-                       intensity = raw_data$intensity, 
-                       sf = input$md_nom/input$md_exact, 
-                       step = input$Y_step,stepsd1=input$Y1_sd, 
-                       stepsd2=input$Y2_sd,
-                       mzc=input$X_change_sd,
-                       cutoffint = input$threshold_int_min, 
-                       cutoffr=1/input$threshold_diff, 
-                       rt = raw_data$rt, 
-                       clustercf = input$cluster_cf
-      )
-      return(t)
-      
-    }else{
       raw_data <- raw_data()
       t <- findhalo_no_rt(mz = raw_data$mz,
                           intensity = raw_data$intensity,
@@ -260,14 +179,12 @@ server <- function(input, output, session){
                           cutoffr=1/input$threshold_diff,
                           clustercf = input$cluster_cf)
       return(t)
-    }
+    
   })
   
   
   # OBSERVEEVENT #
   observeEvent(input$click_file, {
-    
-    
     
     
     ## PLOTTING OUTPUT##
@@ -283,109 +200,42 @@ server <- function(input, output, session){
     ###Plot###
     output$plot1 <- renderPlotly({
       
+      if (input$norm_int) {
+        df <- result()
+        df$intensity <- round(df$intensity/max(df$intensity)*100, 1)
+      } else if (input$norm_int & input$check_overlay){
+        df <- result()
+        df$intensity <- round(df$intensity/max(df$intensity)*100, 1)
+        data2$intensity <- round(df$intensity/max(df$intensity)*100, 1)
       
-      if(input$check_rt){
+      } else if (input$check_overlay){
         df <- result()
-        
-        if (input$check_int) {
-          p <- df %>%
-            plot_ly(
-              x = df$sm,
-              y = df$sd,
-              type = "scatter",
-              size = df$intensity,
-              color = df$i,
-              hovertext = paste("RT: ", df$rt, "\n",
-                                "mz: ", df$mz)
-            )
-        } else {
-          p <- df %>%
-            plot_ly(
-              x = df$sm,
-              y = df$sd,
-              type = "scatter",
-              color = df$i,
-              hovertext = paste("RT: ", df$rt, "\n",
-                                "mz: ", df$mz))
-        }
-        
-        if (input$check_overlay) {
-          p <- p %>%
-            add_markers(x = ~data2$sm, y = ~data2$sd, 
-                        color = I("grey"), opacity = 0.2, hovertext = paste("RT: ", df$rt, "\n",
-                                                                            "mz: ", df$mz))
-        } else {
-          p <- p
-        }
-        
-        if (input$check_overlay & input$check_int) {
-          p <- df %>%
-            plot_ly(
-              x = df$sm,
-              y = df$sd,
-              type = "scatter",
-              size = df$intensity,
-              color = df$i,
-              hovertext = paste("RT: ", df$rt, "\n",
-                                "mz: ", df$mz)
-            ) %>%
-            add_markers(x = ~data2$sm, y = ~data2$sd, 
-                        color = I("grey"), opacity = 0.2, size = ~data2$intensity, hovertext = paste("RT: ", df$rt, "\n",
-                                                                                                     "mz: ", df$mz))
-        } else {
-          p <- p
-        }
-        
-        
-      }else{
-        
+    
+      } else {
         df <- result()
-        
-        if (input$check_int) {
-          p <- df %>%
-            plot_ly(
-              x = df$sm,
-              y = df$sd,
-              type = "scatter",
-              size = df$intensity,
-              color = df$i)
-        } else {
-          p <- df %>%
-            plot_ly(
-              x = df$sm,
-              y = df$sd,
-              type = "scatter",
-              color = df$i)
-        }
-        
-        if (input$check_overlay) {
-          p <- p %>%
-            add_markers(x = ~data2$sm, y = ~data2$sd, 
-                        color = I("grey"), opacity = 0.2)
-        } else {
-          p <- p
-        }
-        
-        if (input$check_overlay & input$check_int) {
-          p <- df %>%
-            plot_ly(
-              x = df$sm,
-              y = df$sd,
-              type = "scatter",
-              size = df$intensity,
-              color = df$i
-            ) %>%
-            add_markers(x = ~data2$sm, y = ~data2$sd, 
-                        color = I("grey"), opacity = 0.2, size = ~data2$intensity)
-        } else {
-          p <- p
-        }
-        
-        
       }
       
-      
-      
+
+       p <- df %>%
+            plot_ly(
+              x = df$mz,
+              y = df$intensity,
+              color = I("red"),
+              type = "bar")
+       
+       if (input$check_overlay) {
+         
+         p <- p %>%
+           add_bars(x = ~data2$mz, 
+                       y = ~data2$intensity, 
+                       color = I("grey"), 
+                       opacity = 0.6)
+       
+       } else {
+         p <- p
+       }
+        
+        
       
     })
     
