fad56bc5a519bb9e816cb515aaabe4a10a1c40dc
WBS-TW <37210875+WBS-TW@users.noreply.github.com>
2022-06-07 16:38:35 +0200

    updated HRMF

--- R/HRMF.R
+++ R/HRMF.R
@@ -1,14 +1,21 @@
-# Actually, using Sirius can help predict and annotate all peaks.
+# Check if any better using Sirius?
+
+
 
 # TODO
 # 1. first Generate the monoisotopic mass -> HRMF monoiso score
 # 2. then add isotope patter from isopattern -> HRMF full iso score
+# 2.1 Add rel ab cutoff to remove low theoretical isotopologues, (default 1% ?). Do this in isopattern
 # 3. add score on isotopic match RMS -> % iso match score
 # 4. Combined score 
+# scores https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8779335/
 
 # 5. loop through top n candidates? (how to get this? from NIST? or some external file generated from NIST search)
 # 6. loop through all names in msp file. How to get annotated formula?
+# 7. Use the theoretical mz of all isotopes as input to xcms to perform new peak picking?
 
+# Filter absolute intensities >50000?
+# Inputs: msp with multiple msp, a csv with different chemical formula to query for each individual msp (nrow should be same as number of msps)
 
 # use rcdk and isopattern
 # https://pubs.acs.org/doi/full/10.1021/acs.analchem.5b01503
@@ -21,29 +28,50 @@ library(devtools)
 library(dplyr)
 library(enviPat)
 library(stringr)
+library(tidyr)
 source("./R/read_msp.R")
 data("isotopes")
+
+# the read_msp was sourced. Change this when finalizing the function
+# Need to manually input formula or extract from msp file? Since unknowns are not annotated with chemical formula and not shown in msp
+
+#msp <- "./data/2-Methoxy-5-methylaniline.msp"
+#formula <- "C8H11NO"
+
+msp <- "./data/MTM00088_GC-EI-FT_POSITIVE_YATIGPZCMOYEGE-UHFFFAOYSA-N.msp"
+formula <- "C14H8Br6O2"
+
+
+# msp <- "./data/MTM00126_GC-EI-FT_POSITIVE_WYEHFWKAOXOVJD-UHFFFAOYSA-N.msp"
+# formula <- "C19H11F5N2O2"
+
+compound <- read_msp(msp)[[1]] %>% select(mz, intensity)
+rownames(compound) <- NULL
+
 
 # state the mass accuracy in ppm
 mass_accuracy <- 10
 
-compound <- read_msp("./data/2-Methoxy-5-methylaniline.msp")[[1]] %>%
-  select(mz, intensity)
-rownames(compound) <- NULL
+# the windows, in Da, defining the +- Da window from which the accurate mass is based on for setting the mass accuracy
+windows <- mass_accuracy/10^6*compound$mz
 
-formula <- "C8H11NO"
+compound <- compound %>%
+  mutate(mz_min = mz-windows,
+         mz_max = mz+windows) %>%
+         select(mz, mz_min, mz_max, intensity)
 
+# using rcdk to generate all elements in the chemical formula
 atoms <- as.data.frame(rcdk::get.formula(formula)@isotopes) %>%
   mutate(number = as.numeric(number)) %>%
   mutate(mass = as.numeric(mass))
 
+# define the minimum (zero) and maximum elements for rcdk to match theoretical formula with measured masses 
 element_range <- list()
 for (i in 1:nrow(atoms)) {
   element_range[[i]] <- c(atoms[i,][[1]], 0, atoms[i,][[2]])
   }
 
-# the windows, in Da, state the +- window from the accurate mass based on the setting on mass accuracy
-windows <- mass_accuracy/10^6*compound$mz
+# Calculating the number of matched peaks
 
 # initiate an empty list
 match_comp <- list()
@@ -59,12 +87,12 @@ match <- rcdk::generate.formula(
   charge = 1
 )
 
-match_list <- list(found = logical(), annodf = data.frame(), isopat = data.frame())
+match_list <- list(annotated = logical(), annodf = data.frame(), isopat = data.frame())
 
 if(length(match) > 0) {
-match_list[["found"]] <- TRUE
+match_list[["annotated"]] <- TRUE
 
-annodf <- data.frame(Mass = match[[1]]@mass,
+annodf <- data.frame(MonoIsoMass = match[[1]]@mass,
                      Formula = match[[1]]@string,
                      MassError_ppm = round((compound$mz[i]-match[[1]]@mass)/match[[1]]@mass*10^6, 1))
 match_list[["annodf"]] <- annodf
@@ -72,21 +100,24 @@ match_list[["annodf"]] <- annodf
 chemforms <- str_extract(match_list[["annodf"]][["Formula"]], "(?<=\\[).+?(?=\\])")
 
 isopat <- as.data.frame(isopattern(isotopes = isotopes, chemforms = chemforms,
-                                   threshold = 0.001, 
+                                   threshold = 1, # this one needs to be used as input variable in function
                                    charge = 1, 
                                    emass = 0.00054858, 
                                    plotit = FALSE, 
                                    algo=1, 
                                    rel_to = 0,
                                    verbose = TRUE)) %>%
-  rename(Mass = 1,
-         Abundance = 2)
+  rename(IsoMass = 1, Abundance = 2) %>%
+  mutate(Abundance = round(Abundance, 1)) %>%
+  mutate(MonoIsoFormula = chemforms) %>%
+  select(MonoIsoFormula, everything()) %>%
+  rename_with( ~str_remove(., paste0(chemforms, ".")), .cols = -(1:3))
 
 match_list[["isopat"]] <- isopat
 
 
 } else {
-  match_list[["found"]] <- FALSE
+  match_list[["annotated"]] <- FALSE
   match_list[["annodf"]] <- NULL
   match_list[["isopat"]] <- NULL
 }
@@ -94,12 +125,70 @@ match_list[["isopat"]] <- isopat
 match_comp[[i]] <- match_list
 }
 
+# binding all objects in the list
+all_ions <- bind_rows(match_comp)
+# This provides all theoretical ions
+all_ions <- tidyr::unnest(all_ions, cols = c(annotated, annodf, isopat)) %>%
+  ungroup() %>%
+  filter(annotated == TRUE) %>%
+  mutate(across(where(is.numeric), ~replace_na(.x, 0)))
+
+all_elems <- all_ions %>%
+  select(-c(1:7)) %>%
+  mutate(IsoFormula = as.character("")) %>%
+  select(`12C`, `13C`, everything())
 
 
+for (i in 1:nrow(all_elems)) {
+  elem <- NULL
+  for (j in 1:ncol(all_elems)) {
+    if(all_elems[i,j] > 0) {
+   elem_iso <- paste0("[", colnames(all_elems[i,j]), "]", all_elems[i,j])
+   elem <- paste0(elem, elem_iso)
+   }
+  }
+  all_elems$IsoFormula[i] <- elem
+}
+
+all_elems <- all_elems %>%
+  select(IsoFormula)
+
+all_ions <- all_ions %>%
+  mutate(Isoformula = all_elems$IsoFormula)
+
+all_ions <- all_ions %>%
+  mutate(Detected_mz = 0) %>%
+  mutate(Detected_int = 0) %>%
+  rename(Annotated = annotated)
+
+for (i in seq_along(all_ions$IsoMass)) {
+  for (j in seq_along(compound$mz)) {
+    
+if(all_ions$IsoMass[i] > compound$mz_min[j] & all_ions$IsoMass[i] < compound$mz_max[j]){
+  all_ions$Detected_mz[i] <- compound$mz[j]
+  all_ions$Detected_int[i] <- compound$intensity[j]}
+  }
+}
 
 
+all_ions <- all_ions %>%
+  mutate(MassError_ppm = case_when(Detected_mz > 1 ~ round((Detected_mz-IsoMass)/IsoMass*10^6,1))) %>%
+  group_by(MonoIsoFormula) %>%
+  mutate(Detected_relab = round(Detected_int/max(Detected_int)*100, 1)) %>%
+  ungroup() %>%
+  select(Annotated:Abundance, Isoformula:Detected_relab)
+  #filter(!is.na(MassError_ppm))
 
-use#' High resolution mass filtering
+
+all_iso <- length(all_ions$MassError_ppm)
+
+HRMF_summary <- all_ions %>%
+  drop_na(MassError_ppm) %>%
+  summarise(det_freq_iso = round(sum(!is.na(MassError_ppm))/all_iso*100, 1),
+            HRF_score = sum(IsoMass*Abundance)/sum(Detected_mz*Detected_relab))
+
+
+#' High resolution mass filtering for GC HRMS data
 #'
 #' @param mass 
 #'
